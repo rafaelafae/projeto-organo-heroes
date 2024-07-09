5579ac7b7e4b5bf1ae772365935624dbf1bae1d3
Rafaela Faé Theodoro <rafaela.fae@gmail.com>
2024-07-09 13:25:28 -0300

    Ajuste footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,18 +4,14 @@ const Footer = () => {
     return (
         // Parece HTML mas não é, é um JSX (Como o react carrega o código e transforma em elementos no DOM)
         <footer className="footer">
-            <div>
-                <img src="/image/Footer-90.png" alt="footer principal da página" className='footer-img'/>
-            </div>
+            <h4>Desenvolvido por Rafaela Faé Theodoro</h4>
             <div className='icons'>
-                <div>
-                    <a href='https://github.com/rafaelafae' >
-                        <img src='/image/github.png' alt='logo github' className='icons-img' />
-                    </a> 
-                    <a href='https://www.linkedin.com/in/rafaelafae/' >
-                        <img src='/image/linkedin.png' alt='logo linkedin' className='icons-img'/>
-                    </a>
-                </div>
+                <a href='https://github.com/rafaelafae' >
+                    <img src='/image/github.png' alt='logo github' className='icons-img' />
+                </a> 
+                <a href='https://www.linkedin.com/in/rafaelafae/' >
+                    <img src='/image/linkedin.png' alt='logo linkedin' className='icons-img'/>
+                </a>
             </div>
         </footer>
     )
